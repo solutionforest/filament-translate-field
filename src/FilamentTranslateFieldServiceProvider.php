0087036796925119bbb5a224ba378c454e39386a
repl6669 <5587309+repl6669@users.noreply.github.com>
2025-08-17 21:10:00 +0000

    Fix styling

--- src/FilamentTranslateFieldServiceProvider.php
+++ src/FilamentTranslateFieldServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace SolutionForest\FilamentTranslateField;
 
-use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Asset;
+use Filament\Support\Assets\Css;
 use Filament\Support\Facades\FilamentAsset;
 use Livewire\Features\SupportTesting\Testable;
 use SolutionForest\FilamentTranslateField\Testing\TestsFilamentTranslateField;
